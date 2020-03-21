b73c6f5e127a54918314178e59554aa33ac05130
david <wu.xiguanghua@163.com>
2020-03-21 18:25:25 +0800

    update sqlserver generic

---
+++ AdvancedNet/Advanced.Sql/SqlServerHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace Advanced.Sql
+{
+    public class SqlServerHelper
+    {
+        private static string ConnectionStr = ConfigurationManager.
+            ConnectionStrings[""].ConnectionString;
+
+        public SqlServerHelper()
+        {
+
+        }
+
+        public void Query()
+        { 
+        
+        }
+
+        /// <summary>
+        /// 泛型查找
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public T Find<T>(int id)
+        {
+            Type type = typeof(T);
+            var sql = $"SELECT {string.Join(",", type.GetProperties().Select(p=>$"[{p.Name}]"))} FROM " +
+                $"[{type.Name}] WHERE Id= {id}";
+            object oObject = Activator.CreateInstance(type);
+            using (SqlConnection conn = new SqlConnection(ConnectionStr))
+            {
+                SqlCommand command = new SqlCommand(sql, conn);
+                conn.Open();
+                var reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    foreach (var prop in type.GetProperties())
+                    {
+                        prop.SetValue(oObject, reader[prop.Name]);
+                    }
+                }
+            }
+            return (T)oObject;
+        }
+
+
+    }
+}
