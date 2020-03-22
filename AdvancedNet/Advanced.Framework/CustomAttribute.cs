eb72b43ecf5ac6f50857d3e56b86a5dfcaa1ad84
david <wu.xiguanghua@163.com>
2020-03-22 10:12:30 +0800

    特性

---
+++ AdvancedNet/Advanced.Framework/CustomAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Advanced.Framework
+{
+    /// <summary>
+    /// 默认是只能用一次attribute，但是设置后也可以用多个特性
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
+    public class CustomAttribute: Attribute
+    {
+        public CustomAttribute()
+        {
+
+        }
+        public CustomAttribute(int id)
+        {
+
+        }
+        public CustomAttribute(string name)
+        {
+
+        }
+
+    }
+}
--- AdvancedNet/AdvancedNet/Models/People.cs
+++ AdvancedNet/AdvancedNet/Models/People.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Advanced.Framework;
 
 namespace AdvancedNet.Models
 {
+    [Custom]
     public class People
     {
         public int Id { get; set; } //property
