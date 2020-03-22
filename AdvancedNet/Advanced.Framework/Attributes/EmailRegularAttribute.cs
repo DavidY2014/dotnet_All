ad1697ddd56da4d7b7a55d7bffd71e34cb851abb
david <wu.xiguanghua@163.com>
2020-03-22 22:38:51 +0800

    expression tree

---
+++ AdvancedNet/Advanced.Framework/Attributes/EmailRegularAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advanced.Framework.Attributes
+{
+    public class EmailRegularAttribute: AbstractValidateAttribute
+    {
+        private static string EmailRegular = @"^\w+([-+.]\w+)*@\w
+                        +([-.]\w+)*\.\w+([-.]\w+)*$";
+
+
+
+        public  override bool Validate(object oValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AdvancedNet/AdvancedNet/Expression/ExpressionTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvancedNet.Expression
+{
+    public class ExpressionTest
+    {
+        public static void Show()
+        { 
+        
+        }
+
+    }
+}
